733d4793ccfc1b33dfedebffec25be32e4f03de5
Marianabaia <maribaiamonteiro@icloud.com>
2025-10-23 19:45:07 +0000

    adiciona scripts`lint:heck` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home () {
-    return <h1> Oi Bruno! Eu, Otavio e Zeca te amamos =) </h1>
+function Home() {
+  return <h1> Oi Bruno! Eu, Otavio e Zeca te amamos =) </h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
